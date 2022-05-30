417f2ecfb39facf27a8cc3003d1420dde9bf3e09
KaivWezel <66356377+KaivWezel@users.noreply.github.com>
2022-05-30 12:20:34 +0200

    Demo attempt

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,21 @@
 <template>
-  <Tutorial />
+  <div>
+    <Tutorial />
+  </div>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
+
+  async asyncData({ $prismic, params, error }) {
+    const document = await $prismic.api.getByUID('Standaard', 'standaard-2')
+
+    if (document) {
+      return { document }
+    } else {
+      error({ statusCode: 404, message: 'Page not found' })
+    }
+  },
 }
 </script>
