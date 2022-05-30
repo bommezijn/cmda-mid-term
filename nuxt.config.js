24ee31df27c15686f62b9da81da00a179396f468
KaivWezel <66356377+KaivWezel@users.noreply.github.com>
2022-05-30 11:55:00 +0200

    connected prismic-repo to nuxt

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+import { apiEndpoint } from './sm.json'
+
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
@@ -30,11 +32,20 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
+    '@nuxtjs/prismic',
   ],
+
+  prismic: {
+    endpoint: apiEndpoint,
+    modern: true,
+    /* see configuration for more */
+  },
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    transpile: ['@prismicio/vue'],
+  },
 }
