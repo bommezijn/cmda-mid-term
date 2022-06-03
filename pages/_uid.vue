34f73904ca90db43dde468b80d770f98ae831a93
KaivWezel <66356377+KaivWezel@users.noreply.github.com>
2022-06-03 11:41:40 +0200

    Created table-slice

--- pages/_uid.vue
+++ pages/_uid.vue
@@ -7,34 +7,40 @@
     </header>
 
     <article v-for="(chapter, index) in document.data.chapters" :key="index">
-    <!-- 1.1 hoofdstuk -->
+      <!-- 1.1 hoofdstuk -->
       <PrismicRichText :field="chapter['chapter-title']" class="title" />
       <section class="content">
         <!-- Content van het hoofdstukje, dus bv foto's animaties, tekst -->
-        <Prismic-image v-if="chapter['img']" :field="chapter['img']"  />
+        <Prismic-image v-if="chapter['img']" :field="chapter['img']" />
         <PrismicRichText :field="chapter['content-of-chapter']" />
       </section>
     </article>
+    <SliceZone :components="components" :slices="document.data.body" />
   </section>
 </template>
 
 <script>
-export default {
-  async asyncData({$prismic, params, error}) {
-    const document = await $prismic.api.getByUID('standaard', params.uid)
+import { components } from '~/slices'
 
+export default {
+  data() {
+    return { components }
+  },
+  async asyncData({ $prismic, params, error }) {
+    const document = await $prismic.api.getByUID('standaard', params.uid)
+    console.log(document)
     if (document) {
-      return {document}
+      return { document }
     } else {
-      error({statuscode: 404, message: 'Page Not Found'})
+      error({ statuscode: 404, message: 'Page Not Found' })
     }
-  }
+  },
 }
 </script>
 
 <style scoped>
-  .content {
-    display: flex;
-    background-color: lightgreen;
-  }
+.content {
+  display: flex;
+  background-color: lightgreen;
+}
 </style>
