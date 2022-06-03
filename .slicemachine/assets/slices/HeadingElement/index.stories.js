d2b3d1aadd56400bb8f43019eb9725943db264be
Nathan B <dewakik@gmail.com>
2022-06-03 11:46:49 +0200

    fix: :poop: Update component placeholders

--- .slicemachine/assets/slices/HeadingElement/index.stories.js
+++ .slicemachine/assets/slices/HeadingElement/index.stories.js
@@ -18,7 +18,7 @@ export const _Default = () => ({
   },
   data() {
     return {
-      mock: {"variation":"default","name":"Default","slice_type":"heading_element","items":[],"primary":{"title":[{"type":"heading1","text":"Optimize efficient users","spans":[]}],"description":[{"type":"paragraph","text":"Eu laboris consectetur amet do tempor id dolore eiusmod irure proident qui elit amet. Esse labore dolore in Lorem aliquip excepteur consequat tempor do sint aliquip et quis. Proident reprehenderit non tempor voluptate.","spans":[]}]},"id":"_Default"}
+      mock: {"variation":"default","name":"Default","slice_type":"heading_element","items":[],"primary":{"title":[{"type":"heading1","text":"Reinvent seamless web-readiness","spans":[]}],"description":[{"type":"paragraph","text":"Laborum consequat quis ut ea ex duis commodo. Qui tempor ea cillum dolor officia aute sunt magna Lorem Lorem fugiat voluptate minim.","spans":[]}]},"id":"_Default"}
     }
   },
   template: '<SliceZone :slices="[mock]" :resolver="resolve" />'
@@ -37,7 +37,7 @@ export const _Subheading = () => ({
   },
   data() {
     return {
-      mock: {"variation":"subheading","name":"subheading","slice_type":"heading_element","items":[],"primary":{"title":[{"type":"heading2","text":"Integrate impactful functionalities","spans":[]}],"description":[{"type":"paragraph","text":"Irure laborum fugiat sunt exercitation pariatur. Elit anim reprehenderit enim esse fugiat adipisicing.","spans":[]}]},"id":"_Subheading"}
+      mock: {"variation":"subheading","name":"subheading","slice_type":"heading_element","items":[],"primary":{"title":[{"type":"heading2","text":"Syndicate revolutionary mindshare","spans":[]}],"description":[{"type":"paragraph","text":"Et esse sint laborum commodo minim excepteur. Tempor enim duis enim ullamco aute deserunt. Labore magna deserunt anim ea aliqua consequat.","spans":[]}]},"id":"_Subheading"}
     }
   },
   template: '<SliceZone :slices="[mock]" :resolver="resolve" />'
