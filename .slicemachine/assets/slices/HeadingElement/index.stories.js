2e0992a391e1d217151d3165a817d7fb3e1a19f5
Nathan B <dewakik@gmail.com>
2022-06-03 11:32:40 +0200

    feat: :lipstick: Update global styling with new variables

--- .slicemachine/assets/slices/HeadingElement/index.stories.js
+++ .slicemachine/assets/slices/HeadingElement/index.stories.js
@@ -18,9 +18,28 @@ export const _Default = () => ({
   },
   data() {
     return {
-      mock: {"variation":"default","name":"Default","slice_type":"heading_element","items":[],"primary":{"title":[{"type":"heading1","text":"Embrace granular synergies","spans":[]}],"description":[{"type":"paragraph","text":"Culpa non laboris dolore ad nostrud mollit ad cillum commodo anim duis.","spans":[]}]},"id":"_Default"}
+      mock: {"variation":"default","name":"Default","slice_type":"heading_element","items":[],"primary":{"title":[{"type":"heading1","text":"Optimize efficient users","spans":[]}],"description":[{"type":"paragraph","text":"Eu laboris consectetur amet do tempor id dolore eiusmod irure proident qui elit amet. Esse labore dolore in Lorem aliquip excepteur consequat tempor do sint aliquip et quis. Proident reprehenderit non tempor voluptate.","spans":[]}]},"id":"_Default"}
     }
   },
   template: '<SliceZone :slices="[mock]" :resolver="resolve" />'
 })
 _Default.storyName = 'Default'
+
+export const _Subheading = () => ({
+  components: {
+    MyComponent,
+    SliceZone
+  },
+  methods: {
+    resolve() {
+      return MyComponent
+    }
+  },
+  data() {
+    return {
+      mock: {"variation":"subheading","name":"subheading","slice_type":"heading_element","items":[],"primary":{"title":[{"type":"heading2","text":"Integrate impactful functionalities","spans":[]}],"description":[{"type":"paragraph","text":"Irure laborum fugiat sunt exercitation pariatur. Elit anim reprehenderit enim esse fugiat adipisicing.","spans":[]}]},"id":"_Subheading"}
+    }
+  },
+  template: '<SliceZone :slices="[mock]" :resolver="resolve" />'
+})
+_Subheading.storyName = 'subheading'
