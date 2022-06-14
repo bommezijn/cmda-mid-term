2241c075d8d8e60794754183fdf6069b79c55164
Fabienne02 <74105233+Fabienne02@users.noreply.github.com>
2022-06-14 09:54:15 +0200

    quotecomp

---
+++ components/QuoteComp.vue
@@ -0,0 +1,169 @@
+<template>
+    <section>
+        <div class="color-picker">
+            <input type="color" id="colorWell" value="#e66465">
+            <label for="colorWell"><img
+                    src="https://images.prismic.io/cmd-midterm/5aa4c117-b663-4f09-9144-1c33b6798026_colorpicker.png?auto=compress,format"></label>
+        </div>
+        <div class="semi-collom-up">"</div>
+        <div class="bubble">Feedback speelt vanaf de start van de opleiding een belangrijke rol: feedback van docenten,
+            peer-feedback als werkvorm (al dan niet via DLO) en CMD student-assistenten die studenten ondersteunen en
+            feedback geven. Het geven van effectieve feedback is niet eenvoudig. Dit is een belangrijke reden om te
+            experimenteren met programmatisch toetsen. Door programmatisch toetsen wordt de rol van feedback – het
+            formatief handelen – extra belangrijk. De studenten zullen nog steeds voornamelijk een product maken, maar
+            ze zullen zich daarbij meer bewust moeten worden van hun eigen ontwerpproces en het bredere
+            (maatschappelijke) belang ervan.</div>
+        <img src="https://images.prismic.io/cmd-midterm/407653ef-ca7e-4d2b-975b-5b1164f0d369_user.png?auto=compress,format"
+            class="sanne">
+        <div class="semi-collom-down">"</div>
+        <div class="BackBubble">
+            Feedback speelt vanaf de start van de opleiding een belangrijke rol: feedback van docenten, peer-feedback
+            als werkvorm (al dan niet via DLO) en CMD student-assistenten die studenten ondersteunen en feedback geven.
+            Het geven van effectieve feedback is niet eenvoudig. Dit is een belangrijke reden om te experimenteren met
+            programmatisch toetsen. Door programmatisch toetsen wordt de rol van feedback – het formatief handelen –
+            extra belangrijk. De studenten zullen nog steeds voornamelijk een product maken, maar ze zullen zich daarbij
+            meer bewust moeten worden van hun eigen ontwerpproces en het bredere (maatschappelijke) belang ervan.
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'CardSwitch',
+    data() {
+        return {
+            isChecked: false
+        }
+    },
+    methods: {
+        CardConColor() {
+            this.isChecked = !this.isChecked
+        }
+    }
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600;700;800&display=swap');
+
+.bubble {
+    position: relative;
+    background: #f8f0d5;
+    color: #000000;
+    font-family: 'Open Sans';
+    font-size: 1em;
+    text-align: center;
+    width: 28em;
+    height: 18em;
+    border-radius: 10px;
+    padding: 5em;
+    border: #000000 solid 2px;
+    z-index: 10;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.bubble:after {
+    content: '';
+    position: absolute;
+    display: block;
+    width: 0;
+    z-index: 1;
+    border-style: solid;
+    border-color: #f8f0d5 transparent;
+    border-width: 66px 16px 0;
+    bottom: -61px;
+    left: 19%;
+    margin-left: -16px;
+}
+
+.bubble:before {
+    content: '';
+    position: absolute;
+    width: 0;
+    z-index: 0;
+    border-style: solid;
+    border-color: #000000 transparent;
+    border-width: 64px 18px 0;
+    bottom: -64px;
+    left: 19%;
+    margin-left: -18px;
+    display: block;
+}
+
+.BackBubble {
+    position: relative;
+    background: #000000;
+    font-family: Arial;
+    font-size: 1em;
+    text-align: center;
+    width: 20em;
+    height: 16em;
+    border-radius: 10px;
+    padding: 5em;
+    top: -25em;
+    left: 11em;
+    transform: scaleX(-1);
+}
+
+.BackBubble:after {
+    content: '';
+    position: absolute;
+    display: block;
+    width: 0;
+    z-index: 1;
+    border-style: solid;
+    border-color: #000000 transparent;
+    border-width: 61px 16px 0;
+    bottom: -61px;
+    left: 19%;
+    margin-left: -16px;
+}
+
+.semi-collom-up {
+    position: absolute;
+    font-family: 'Open Sans';
+    font-size: 9em;
+    z-index: 12;
+    left: 0.3em;
+}
+
+.semi-collom-down {
+    position: absolute;
+    font-family: 'Open Sans';
+    font-size: 9em;
+    z-index: 12;
+    top: 1.9em;
+    left: 3.4em;
+    transform: scaleY(-1);
+}
+
+.sanne {
+    position: absolute;
+    height: 7em;
+    z-index: 13;
+    left: 16em;
+    top: 22em;
+}
+
+.color-picker img {
+    position: absolute;
+    height: 6em;
+    z-index: 13;
+    left: 32em;
+    top: 0.4em;
+    transition: transform .2s;
+}
+
+.color-picker img:hover {
+    transform: scale(1.2);
+}
+
+input {
+    position: absolute;
+    visibility: hidden;
+    left: 32em;
+    top: 5em;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
-    <CardSwitch></CardSwitch>
+    <!-- <CardSwitch /> -->
+    <QuoteComp />
   </div>
 </template>
 
 <script>
+import QuoteComp from '~/components/QuoteComp.vue';
 export default {
-  name: 'IndexPage',
+  name: "IndexPage",
+  components: { QuoteComp }
 }
 </script>
