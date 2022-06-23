879111b1c98bc192b1d50066e32f8be3eea07f1a
PipHarsveld <pip.harsveld@outlook.com>
2022-06-23 12:53:49 +0200

    chore: eslint

---
+++ components/footerComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <section>
+        <p>&#169; 2022 CMD AMSTERDAM</p>
+        <p>Gerealiseerd door:</p>
+        <ul>
+            <li>Alana Eastman</li>
+            <li>Nina Rijntjes</li>
+            <li>Pip Harsveld</li>
+            <li>Ryan Zoeters</li>
+        </ul>
+        <ul>
+            <li>Fabiene van den steen</li>
+            <li>Kai van Wezel</li>
+            <li>Nathan Bommezijn</li>
+        </ul>
+    </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+section {
+    background-color: #595959;
+}
+
+</style>
